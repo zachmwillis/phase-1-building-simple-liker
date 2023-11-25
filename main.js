e20ea42756c7b5b10bf30afafa4772521de846d9
Zach Willis <zachmw27@gmail.com>
2023-11-24 23:37:54 -0500

    complete

--- main.js
+++ main.js
@@ -3,7 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", function() {
+  const errorModal = document.getElementById("modal");
+  errorModal.classList.add("hidden");
 
+  const hearts = document.querySelectorAll(".like-glyph");
+
+  function handleHeartClick(event) {
+    const heart = event.target;
+
+    mimicServerCall()
+      .then(() => {
+        heart.innerText = FULL_HEART;
+        heart.classList.add("activated-heart");
+      })
+      .catch((error) => {
+        errorModal.classList.remove("hidden");
+        const modalMessage = document.getElementById("modal-message");
+        modalMessage.innerText = error;
+        setTimeout(() => {
+          errorModal.classList.add("hidden");
+        }, 3000);
+      });
+  }
+
+  hearts.forEach((heart) => {
+    heart.addEventListener("click", handleHeartClick);
+  });
+});
 
 
 
